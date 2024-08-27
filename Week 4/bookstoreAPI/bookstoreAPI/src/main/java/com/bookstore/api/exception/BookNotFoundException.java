b5370b3b37fa831860e63693278e7f90b6a32840
PoojaNayak2 <95607283+PoojaNayak2@users.noreply.github.com>
2024-08-27 10:53:11 +0530

    Delete Week 4/bookstoreAPI/bookstoreAPI directory

--- Week 4/bookstoreAPI/bookstoreAPI/src/main/java/com/bookstore/api/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.api.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
